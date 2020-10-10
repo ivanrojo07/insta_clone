a0094abcf2a37d85e0abf1d5d1b1f2ad1fa5aab8
Ivan Rojo <ivanrojo07@gmail.com>
2020-10-09 20:11:04 -0500

    Comentar, like y eliminar post

--- routes/post.js
+++ routes/post.js
@@ -1,4 +1,3 @@
-const { response } = require("express")
 const express = require("express")
 const router = express.Router()
 const mongoose = require("mongoose")
@@ -7,6 +6,7 @@ const Post =mongoose.model("Post")
 
 router.get("/all_post",requireLogin,(req,res)=>{
     Post.find().populate("postedBy","_id name")
+    .populate("comments.postedBy","_id name")
         .then(posts=>{
             return res.status(200).json({posts:posts})
         })
@@ -43,6 +43,7 @@ router.get("/my_post", requireLogin, (req,res)=>{
     console.log(req.id)
     Post.find({postedBy:req.id})
         .populate("postedBy","_id name")
+        .populate("comments.postedBy","_id name")
         .then(posts=>{
             // console.log(posts)
             return res.status(200).json({posts:posts})
@@ -50,5 +51,86 @@ router.get("/my_post", requireLogin, (req,res)=>{
         .catch(err=>{
             return res.status(422).json({error:err})
         })
+})
+
+router.put("/like", requireLogin,(req,res)=>{
+    Post.findByIdAndUpdate(req.body.postId,{
+        $push:{
+            likes:req.id
+        }
+    },
+    {
+        new:true
+    }).populate("postedBy","_id name").populate("comments.postedBy","_id name").exec((err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }
+        else{
+            return res.status(200).json(result)
+        }
+    })
+})
+
+router.put("/unlike", requireLogin,(req,res)=>{
+    Post.findByIdAndUpdate(req.body.postId,{
+        $pull:{
+            likes:req.id
+        }
+    },
+    {
+        new:true
+    }).populate("postedBy","_id name").populate("comments.postedBy","_id name").exec((err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }
+        else{
+            return res.status(200).json(result)
+        }
+    })
+})
+
+router.put("/comment", requireLogin,(req,res)=>{
+    if(!req.body.text){
+        return response.json(422).json({error:"Text required"})
+    }
+    const comment = {
+        text:req.body.text,
+        postedBy:req.id
+    }
+
+    Post.findByIdAndUpdate(req.body.postId,{
+        $push:{
+            comments:comment
+        }
+    },{
+        new:true
+    }).populate("postedBy","_id name").populate("comments.postedBy","_id name").exec((error,result)=>{
+        if(error){
+            return res.status(422).json(error)
+        }
+        else{
+            return res.status(201).json(result)
+        }
+    })
+})
+
+router.delete("/deleted_post/:postId",requireLogin,(req,res)=>{
+    Post.findOne({_id:req.params.postId})
+    .populate("postedBy","_id name").populate("comments.postedBy","_id name")
+    .exec((err,post)=>{
+        if (err || !post){
+            return res.status(422).json({error:err})
+        }
+        if(post.postedBy._id.toString() === req.id.toString()){
+            post.remove().then(result=>{
+                return res.status(200).json(result)
+            }).catch(err=>{
+                console.log(err)
+            })
+        }
+        else{
+            return res.status(422).json({error:"You not have the permission to delete this post"})
+        }
+    })
 })
 module.exports = router
